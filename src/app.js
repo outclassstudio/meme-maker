f4269c0be68024ac34e8421710a0bd34d77d1408
outclass <mhlee@wemb.co.kr>
2023-01-03 14:16:11 +0900

    Feat: Set color preset

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const lineWidthInput = document.querySelector("#line-width")
 const lineWidthValue = document.querySelector("#line-width-value")
 const colorSetInput = document.querySelector("#color-set")
 const colorValue = document.querySelector("#color-value")
+const colorPaletteContainer = document.querySelector("#color-palette")
+const colorPalette = ["#1abc9c", "#3498db", "#34495e", "#27ae60", "#8e44ad", "#f1c40f", "#e74c3c", "#95a5a6", "#d35400", "#bdc3c7", "#2ecc71", "#e67e22"]
 
 canvas.width = 800
 canvas.height = 800
@@ -36,10 +38,27 @@ function onLineWidthChange(e) {
 }
 
 function onColorChange (e) {
-  colorValue.textContent = e.target.value
-  ctx.strokeStyle = e.target.value
-  ctx.fillStyle = e.target.value
+  const colorValue = e.target.value
+  colorValue.textContent = colorValue
+  ctx.strokeStyle = colorValue
+  ctx.fillStyle = colorValue
 }
+
+function selectPresetColor (e) {
+  const colorValue = e.target.dataset.color
+  ctx.strokeStyle = colorValue
+  ctx.fillStyle = colorValue
+  colorSetInput.value = colorValue
+}
+
+colorPalette.forEach((color) => {
+  let div = document.createElement("div")
+  div.setAttribute("class", "palette-children")
+  div.setAttribute("data-color", color)
+  div.style.background = color;
+  colorPaletteContainer.appendChild(div)
+  div.addEventListener("click", selectPresetColor)
+})
 
 canvas.addEventListener("mousedown", mouseDown)
 canvas.addEventListener("mouseup", mouseUp)
